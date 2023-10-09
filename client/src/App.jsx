88409234cda0cd2b37fea5a2d67dfe15043d9809
amro1230 <amro_1230@yahoo.com>
2023-10-09 11:58:55 -0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+
+import Customizer from './pages/Customizer';
+import Home from "./pages/Home";
+import Canvas from './canvas/Index';
+
+function App() {
+  return (
+    
+      <main className="app transition-all ease-in">
+        <Home />
+        <Canvas />
+        <Customizer />
+      </main>
+ 
+  )
+}
+
+export default App
